fd16f45c3a8fa0b7b5fa48977b109da1a0d68738
Arruda98 <jeffersonarruda98@gmail.com>
2025-04-21 19:32:55 -0300

    Add files via upload

---
+++ Calculator.API/Calculator.API/Calculator.cs
@@ -0,0 +1,38 @@
+﻿namespace Calculator.API
+{
+    public class Calculator
+    {
+        public double NumberOne { get; set; }
+        public double NumberTwo { get; set; }
+
+        public double Somar()
+        {
+            return NumberOne + NumberTwo;
+        }
+
+        public double Multiplicar()
+        {
+            return NumberOne * NumberTwo;
+        }
+
+        public double Dividir()
+        {
+            return NumberOne / NumberTwo;
+        }
+
+        public double Subtrair()
+        {
+            return NumberOne - NumberTwo;
+        }
+
+        public double Potencia()
+        {
+            return Math.Pow(NumberOne, NumberTwo);
+        }
+
+        public double Porcentagem()
+        {
+            return (NumberTwo * NumberOne) / 100;
+        }
+    }
+}
---
+++ Calculator.API/Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController
+    {
+        private static readonly string[] Operations = new[]
+        {
+            "Somar", "Multiplicar", "Subtrair", "Dividir", "Potencia"
+        };
+
+
+        [HttpPost("Operacao")]
+        public string Calculate(double numberOne, double numberTwo, string operation)
+        {
+            var calculator = new Calculator()
+            {
+                NumberOne = numberOne,
+                NumberTwo = numberTwo,
+            };
+
+            switch(operation.ToLower())
+            {
+                case "somar": return $"Resultado: {calculator.Somar()}";
+                case "multiplicar": return $"Resultado: {calculator.Multiplicar()}";
+                case "subtrair": return $"Resultado: {calculator.Subtrair()}";
+                case "dividir": return $"Resultado: {calculator.Dividir()}";
+                case "potencia": return $"Resultado: {calculator.Potencia()}";
+                default: return "Operação invalida.";
+            }
+        }
+
+        [HttpGet("ListarOperacoes")]
+        public IEnumerable<object> ListOperations()
+        {
+            return Enumerable.Range(0, 5).Select(i => new
+            {
+                Operation = Operations[i]
+            }).ToArray();
+        }
+
+        [HttpPost("Porcentagem")]
+        public string Porcentagem(double valor, double porcentagem)
+        {
+            var calculator = new Calculator()
+            {
+                NumberOne = valor,
+                NumberTwo = porcentagem,
+            };
+
+            return $"{porcentagem}% de {valor} é igual a: {calculator.Porcentagem()}";
+        }
+    }
+
+}
